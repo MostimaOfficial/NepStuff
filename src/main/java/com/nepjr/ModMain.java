a3bbecdf308bfedbea37f1d127641a6d40c7d655
WarriorUnit <diehardsteam@gmail.com>
2020-01-25 15:39:55 -0800

    Added Stuff

--- src/main/java/com/nepjr/ModMain.java
+++ src/main/java/com/nepjr/ModMain.java
@@ -1,18 +1,19 @@
-package com.example.examplemod;
+package com.nepjr;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ModMain.MODID, name = ModMain.NAME, version = ModMain.VERSION)
+public class ModMain
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "nepstuff";
+    public static final String NAME = "Nep Stuff";
+    public static final String VERSION = "1.0-rv1";
 
     private static Logger logger;
 
@@ -25,7 +26,11 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    	
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    	
     }
 }
